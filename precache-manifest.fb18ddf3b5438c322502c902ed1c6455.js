3192264af17147fa65179ad390c004efaab55b6d
Jorge Garnica B <jorgegarba@gmail.com>
2020-05-30 09:08:12 -0500

    Subiendo empaquetado de AgendaApp

---
+++ precache-manifest.fb18ddf3b5438c322502c902ed1c6455.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697b272119d8fb384951606f12a20106",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af858a5165bd28fafd1",
+    "url": "/static/js/2.1a4cdf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a4cdf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd3a33a5c11d65fc741",
+    "url": "/static/js/main.e18b3777.chunk.js"
+  },
+  {
+    "revision": "e275a57e96d358f64ebf",
+    "url": "/static/js/runtime-main.6dfc73c8.js"
+  }
+]);
